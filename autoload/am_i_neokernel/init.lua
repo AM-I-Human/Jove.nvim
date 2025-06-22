380ee59b6b970fd9219fdeac80ee1d734e259c44
Am-I-Human <andrea.marisio.am@gmail.com>
2025-06-22 16:32:15 +0200

    Certamente! Per completare l'integrazione dei nuovi comandi e la corretta configurazione del plugin, è necessario aggiornare il file `autoload\am_i_neokernel\init.lua` con le modifiche descritte. ```

--- autoload/am_i_neokernel/init.lua
+++ autoload/am_i_neokernel/init.lua
@@ -1,21 +1,19 @@
 -- init.lua
-local kernel = require("jupytex.kernel")
+-- Questo file inizializza il plugin am_i_neokernel.
 
-vim.api.nvim_create_user_command("JupyterStart", function(opts)
-	kernel.start(opts.args)
-end, { nargs = 1, desc = "Start a Jupyter kernel" })
-
-vim.api.nvim_create_user_command("JupyterExecute", function()
-	local bufnr = vim.api.nvim_get_current_buf()
-	local row = vim.api.nvim_win_get_cursor(0)[1] - 1
-	local lines = vim.api.nvim_buf_get_lines(bufnr, row, row + 1, false)
-	local cell_content = lines[1]
-	kernel.execute_cell("python3", cell_content) --  "python3" for now
-end, { desc = "Execute the current cell" })
-
--- Example configuration (you can move this to a separate config file later)
-vim.g.jupytex_kernels = {
-	python3 = {
-		cmd = "python3 -m ipykernel_launcher -f {connection_file}",
+-- Configurazione di esempio per i kernel.
+-- Questa variabile globale può essere sovrascritta dall'utente nel suo init.lua.
+vim.g.am_i_neokernel_kernels = {
+	python = {
+		cmd = "python -m ipykernel_launcher -f {connection_file}",
+		-- Puoi aggiungere altre configurazioni qui, es. display_name
 	},
+	-- Aggiungi altri kernel se necessario
 }
+
+-- Carica i comandi utente definiti per il plugin.
+-- Questi comandi sono ora definiti in lua/am_i_neokernel/commands.lua
+require("am_i_neokernel.commands")
+
+-- Nota: I vecchi comandi JupyterStart e JupyterExecute sono stati rimossi da qui
+-- e spostati in lua/am_i_neokernel/commands.lua con i nomi AmINeoKernelStart e AmINeoKernelExecute.

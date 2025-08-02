744a38479d7ca0eb5f40db7f9e6eb097fc9a059c
Am-I-Human <marisio.andrea@yahoo.it>
2025-08-02 17:26:57 +0200

    removing comments

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
 import pandas as pd
 from PIL import Image
 
-# Per visualizzare un'immagine in un ambiente Jupyter (come Jove),
-# l'oggetto immagine deve essere l'ultima espressione valutata nella cella.
-# Il metodo .show() apre il visualizzatore di immagini predefinito del sistema operativo,
-# che non è quello che vogliamo per l'integrazione nel terminale.
-
-# Assicurati che il percorso dell'immagine sia corretto.
 Image.open(r"C:\Users\andre\Pictures\Andrea\Small_SALOTTO-CULTURALE_MG_6719.jpg")
-
 # Le righe seguenti sono state commentate per isolare il test di visualizzazione dell'immagine.
 # Se non commentate, l'output di una di queste righe sovrascriverebbe l'immagine.
 #
